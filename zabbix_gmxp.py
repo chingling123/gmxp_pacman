a6380956fec289dd892b2e605ace32f312e54b16
Erik <erik@2eg.com.br>
2018-08-23 13:42:11 -0300

    zabbix

---
+++ zabbix_gmxp.py
@@ -0,0 +1,10 @@
+from pyzabbix import ZabbixMetric, ZabbixSender
+# zabbix-srv.brazilsouth.cloudapp.azure.com
+
+class SendDataZabbix:
+    def send_zabbix(self, localhost, item, data):
+        packet = [
+            ZabbixMetric(localhost, item, data),
+        ]
+        result = ZabbixSender(zabbix_server='192.168.30.199',zabbix_port=10051,use_config=None).send(packet)
+        print result
